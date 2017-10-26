442c0a55f52b4d4fe9a8697365b05edf7e8ba050
Elias011 <elias.shkshb1991@gmail.com>
2017-10-26 15:44:22 +0200

    added more probable outcomes for x and o

--- Game.js
+++ Game.js
@@ -24,26 +24,3 @@ Game.prototype.getWinner = function () {
 
 module.exports = Game;
 
-
-
-
-
-
-/*game.prototype.getBoardString = function () {
- -Game.prototype.getBoardArray = function () {		 +     let boardString = '';
- -    return this.board.trim().split("\n").map(row => row.trim());		 +     for (i of this.board){
- -};		 +          boardString += i.trim().split("\n").map(row => row.trim()).join('');
- -		 +     }
- -Game.prototype.getWinner = function () {		 +     return boardString;
- -    return 1;		 +}
- -};		 +game.prototype.getWinner = function () {
- -		 +     if (this.getBoardString() == '--x-x-x--xxx---------xxx---------xxxx---x---x')
- -module.exports = Game;		 +          return 1;
-     if (this.getBoardString() == '--o-o-o--ooo---------ooo---------oooo---o---o')
-           return 2;
- }
- game.prototype.getGameBoard = function () {
-      if (this.getBoardString() == '---------')
-           return null;
- }
- module.exports = game;*/
